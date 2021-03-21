6949607891a2152ba6bf9d80ce601b37f5e52d86
drivendragon <drivendragon@hotmail.com>
2021-03-21 19:46:15 -0400

    validations

--- courses_app/models.py
+++ courses_app/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['name']) < 5:
+            errors["name"] = "Name should be at least 5 characters"
+        if len(postData['description']) < 10:
+            errors["desc"] = "Description should be at least 10 characters"
+        return errors
+
+
 class Course(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-   # objects = ShowManager()
+    objects = ShowManager()
 
--- courses_app/views.py
+++ courses_app/views.py
@@ -10,10 +10,17 @@ def course(request):
   return render(request, "index.html", context)
 
 def new_course(request): #applies when hitting Add on main page
-  #if request.method =="POST":
-  newly_created_course = Course(name=request.POST['name'], description=request.POST['description'])
-  newly_created_course.save()
- # return redirect(f"/course/{newly_created_course.id}")
+  if request.method == "POST":
+      errors = Course.objects.basic_validator(request.POST)
+      if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+          messages.error(request, value) 
+          return redirect("/")      
+      newly_created_course = Course(name=request.POST['name'], description=request.POST['description'])
+      newly_created_course.save()
+      return redirect("/")
+     # return redirect(f"/course/{newly_created_course.id}")
   return redirect("/")
 
 def destroy(request, id):
@@ -30,3 +37,24 @@ def delete(request,id):  #applies when hitting Delete button on the main page
   return redirect("/")
 
 
+
+  
+#==================================
+#if request.method == "POST":
+#   errors = Shows.objects.basic_validator(request.POST)
+#    if len(errors) > 0:
+#      # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+#      for key, value in errors.items():
+#        messages.error(request, value) 
+#        return redirect("/shows/new")      
+#    newly_created_course = Course(name=request.POST['name'], description=request.POST['description'])
+#    newly_created_course.save()
+#    return redirect(f"/shows/{newly_created_show.id}")
+#  return redirect("/")
+
+#def new_course(request): #applies when hitting Add on main page
+  #if request.method =="POST":
+#  newly_created_course = Course(name=request.POST['name'], description=request.POST['description'])
+#  newly_created_course.save()
+ # return redirect(f"/course/{newly_created_course.id}")
+#  return redirect("/")
